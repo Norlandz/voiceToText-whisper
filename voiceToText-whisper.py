56ca55589c885bd5b6bafece1e27c3a4934a3158
norl <norl@example.com>
2024-05-30 14:29:38 -0400

    use folder

--- voiceToText-whisper.py
+++ voiceToText-whisper.py
@@ -29,8 +29,8 @@ platform_system = platform.system()
 import re
 from dataclasses import asdict
 
-import whisper.model
 import whisper
+import whisper.model
 
 # import ffmpeg
 
@@ -94,79 +94,113 @@ class TranscribeResult(TypedDict):
 
 ############
 
-if platform_system == "Windows": # Local Windows
+if platform_system == "Windows":  # Local Windows
     path_projectDir = path_entryScript_parentDir
-elif platform_system == "Linux": # Google Colab
+elif platform_system == "Linux":  # Google Colab
     path_projectDir = cwd / "voiceToText-whisper"
 else:
     raise Exception("platform not supported")
 
-path_Audio = path_projectDir / "assets/0014163210622 - job viva packaging.mp3"
-# path_Audio = "./assets/test.mp3"
-pathStr_Subtitle = re.sub(r"\.mp3$", ".vtt", path_Audio.__fspath__())
-print(f'{dt.now().strftime("%H:%M:%S")} ' + path_Audio.__fspath__())
 
-model = whisper.load_model("base.en") # small medium
-initial_prompt = """
+def load_model_preconfig(modelSizeAndName: str):
+    #   |>"
+    #   No, you do not need to load the Whisper model for each file. You can load the model once and then use it to process multiple files. Loading the model is typically a time-consuming operation, so reloading it for each file would be inefficient.
+    #   <|
+    #   https://chatgpt.com/c/4cf5d5ba-8ada-4f41-a279-25e8c6dbe394
+    model = whisper.load_model(modelSizeAndName)  # https://github.com/openai/whisper?tab=readme-ov-file#available-models-and-languages
+    initial_prompt = """
 - put each complete sentence into each segment integrally, do not break and put an integral sentence into multiple segments, unless the sentence is too long.
 """
-print(f'{dt.now().strftime("%H:%M:%S")} ' + "model loaded")
+    print(f'{dt.now().strftime("%H:%M:%S")} ' + "model loaded")
 
-oDecodingOptions = whisper.DecodingOptions(
-    # task='transcribe',
-    language="en",
-    # prompt='',
-    # UserWarning: FP16 is not supported on CPU; using FP32 instead   warnings.warn("FP16 is not supported on CPU; using FP32 instead")
-    # fp16=False,  # ... idk, mem note gone
-)
-_transcribe_result = model.transcribe(path_Audio.__fspath__(), word_timestamps=True, initial_prompt=initial_prompt, **asdict(oDecodingOptions))
-transcribe_result: TranscribeResult = _transcribe_result  # type: ignore
-# transcribe_result["text"]
-# transcribe_result["language"]
-print(f'{dt.now().strftime("%H:%M:%S")} ' + "transcribe done")
+    oDecodingOptions = whisper.DecodingOptions(
+        # task='transcribe',
+        language="en",
+        # prompt='',
+        # UserWarning: FP16 is not supported on CPU; using FP32 instead   warnings.warn("FP16 is not supported on CPU; using FP32 instead")
+        # fp16=False,  # ... idk, mem note gone
+    )
 
-content_vtt = """WEBVTT
+    return model, initial_prompt, oDecodingOptions
+
+
+def voiceToText(pathFile_Audio: Path, model: whisper.Whisper, initial_prompt: str, oDecodingOptions: whisper.DecodingOptions):
+    # pathStr_SubtitleVtt = re.sub(r"\.mp3$", ".vtt", pathFile_Audio.__fspath__())
+    pathStr_SubtitleVtt = re.sub(r"(.*)\.(.*?)$", r"\g<1>.vtt", pathFile_Audio.__fspath__())
+    pathStr_SubtitleTxt = re.sub(r"(.*)\.(.*?)$", r"\g<1>.txt", pathFile_Audio.__fspath__())
+
+    print(f'{dt.now().strftime("%H:%M:%S")} ' + pathFile_Audio.__fspath__())
+
+    _transcribe_result = model.transcribe(pathFile_Audio.__fspath__(), word_timestamps=True, initial_prompt=initial_prompt, **asdict(oDecodingOptions))
+    transcribe_result: TranscribeResult = _transcribe_result  # type: ignore
+    # transcribe_result["text"]
+    # transcribe_result["language"]
+    print(f'{dt.now().strftime("%H:%M:%S")} ' + "transcribe done")
+
+    content_vtt = """WEBVTT
 Kind: captions
 Language: en
 """
-# X-TIMESTAMP-MAP=MPEGTS:0,LOCAL:00:00:00.000
-# Region: top
-# Style
-# ::cue {
-#     color: white;
-#     background-color: black;
-#     font-size: 1.2em;
-#     text-align: center;
-# }
+    # X-TIMESTAMP-MAP=MPEGTS:0,LOCAL:00:00:00.000
+    # Region: top
+    # Style
+    # ::cue {
+    #     color: white;
+    #     background-color: black;
+    #     font-size: 1.2em;
+    #     text-align: center;
+    # }
 
+    content_txt = ""
 
-def get_fix_WordSegmentTimeMisAlign(segment_info: SegmentInfo):
-    time_start_segment = segment_info["start"]
-    if segment_info["words"].__len__() == 0:
+    def get_fix_WordSegmentTimeMisAlign(segment_info: SegmentInfo):
+        time_start_segment = segment_info["start"]
+        if segment_info["words"].__len__() == 0:
+            return time_start_segment
+
+        time_start_word_1st = segment_info["words"][0]["start"]
+        # sometimes can happen, forget what align or result thing
+        if time_start_word_1st < time_start_segment:
+            time_start_segment = time_start_word_1st
         return time_start_segment
 
-    time_start_word_1st = segment_info["words"][0]["start"]
-    # sometimes can happen, forget what align or result thing
-    if time_start_word_1st < time_start_segment:
-        time_start_segment = time_start_word_1st
-    return time_start_segment
+    for segment_info in transcribe_result["segments"]:
+        timeVtt_start_segment = format_ToVttTimestamp(get_fix_WordSegmentTimeMisAlign(segment_info))
+        timeVtt_end_segment = format_ToVttTimestamp(segment_info["end"])
+        segment_text = segment_info["text"]
+        segment_id = segment_info["id"]
+        print(f'{dt.now().strftime("%H:%M:%S")} ' + f"segment:: {segment_id:3} {segment_text}")
+        # \n{segment_text[1:] if segment_text[0] == ' ' else segment_text}
+        content_vtt += f"\n\n{segment_id}\n{timeVtt_start_segment} --> {timeVtt_end_segment}\n"
+        content_txt += segment_text + "\n\n"
+        for word_info in segment_info["words"]:
+            timeVtt_start_word = format_ToVttTimestamp(word_info["start"])
+            timeVtt_end_word = format_ToVttTimestamp(word_info["end"])
+            word_text = word_info["word"]
+            content_vtt += f"<{timeVtt_start_word}><c>{word_text}</c>"
+            # sys.exit(0) # DebugBreak
+    print(f'{dt.now().strftime("%H:%M:%S")} ' + "content_vtt done")
+
+    file_vtt = open(pathStr_SubtitleVtt, "w", encoding="utf-8")
+    file_vtt.write(content_vtt)
+    file_txt = open(pathStr_SubtitleTxt, "w", encoding="utf-8")
+    file_txt.write(content_txt)
 
 
-for segment_info in transcribe_result["segments"]:
-    timeVtt_start_segment = format_ToVttTimestamp(get_fix_WordSegmentTimeMisAlign(segment_info))
-    timeVtt_end_segment = format_ToVttTimestamp(segment_info["end"])
-    segment_text = segment_info["text"]
-    segment_id = segment_info["id"]
-    print(f'{dt.now().strftime("%H:%M:%S")} ' + f"segment:: {segment_id:3} {segment_text}")
-    # \n{segment_text[1:] if segment_text[0] == ' ' else segment_text}
-    content_vtt += f"\n\n{segment_id}\n{timeVtt_start_segment} --> {timeVtt_end_segment}\n"
-    for word_info in segment_info["words"]:
-        timeVtt_start_word = format_ToVttTimestamp(word_info["start"])
-        timeVtt_end_word = format_ToVttTimestamp(word_info["end"])
-        word_text = word_info["word"]
-        content_vtt += f"<{timeVtt_start_word}><c>{word_text}</c>"
-        # sys.exit(0) # DebugBreak
-print(f'{dt.now().strftime("%H:%M:%S")} ' + "content_vtt done")
+########
 
-file_vtt = open(pathStr_Subtitle, "w", encoding="utf-8")
-file_vtt.write(content_vtt)
+# filename_Audio = "0014163210622 - job viva packaging.mp3"
+# filename_Audio = "quickfox-test.mp3"
+# pathFolder_AudioToTranscribe = path_projectDir / "assets/AudioToTranscribe"
+# pathFile_Audio = pathFolder_AudioToTranscribe / filename_Audio
+
+########
+
+pathFolder_AudioToTranscribe = path_projectDir / "assets/AudioToTranscribe"
+
+modelSizeAndName = "base.en"  # tiny base small medium large
+model, initial_prompt, oDecodingOptions = load_model_preconfig(modelSizeAndName)
+
+for pathFile_Audio in pathFolder_AudioToTranscribe.iterdir():
+    if pathFile_Audio.is_file():
+        voiceToText(pathFile_Audio, model, initial_prompt, oDecodingOptions)
